9256245c74bf18cb856b52fd13fb0a96501952e9
Aditya Kumar <84377163+thakuradi@users.noreply.github.com>
2024-11-25 14:34:31 +0530

    added

---
+++ src/constants/socials.tsx
@@ -0,0 +1,24 @@
+import {
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandGithub,
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "https://twitter.com/mannupaaji",
+    label: "Twitter",
+    icon: IconBrandTwitter,
+  },
+  {
+    href: "https://linkedin.com/in/manuarora28",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://github.com/thakuradi",
+    label: "GitHub",
+    icon: IconBrandGithub,
+  },
+];
